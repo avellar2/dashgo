df3d27661cf4ee7850984976d6e14f6f77bb6412
Vanderson Avellar <vandimavellar1997@gmail.com>
2021-11-30 22:22:10 -0300

    header finalizado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Icon, Input, Text } from "@chakra-ui/react";
-import {RiSearchLine} from 'react-icons/ri'
+import { Avatar, Box, Flex, HStack, Icon, Input, Stack, Text } from "@chakra-ui/react";
+import {RiNotificationLine, RiSearchLine, RiUserAddLine} from 'react-icons/ri'
 export function Header() {
     return(
         <Flex 
@@ -45,6 +45,35 @@ export function Header() {
                 />
 
                 <Icon as={RiSearchLine} fontSize='20'/>
+            </Flex>
+
+            <Flex
+                ml={'auto'}
+                align={'center'}
+            >
+                <HStack 
+                    spacing='8'
+                    mx={'8'}
+                    pr={'8'}
+                    py={'1'}
+                    color={'gray.300'}
+                    borderRightWidth={1}
+                    borderColor={'gray.700'}
+                >
+                    <Icon as={RiNotificationLine} fontSize={'20'}/>
+                    <Icon as={RiUserAddLine} fontSize={'20'}/>
+                </HStack>
+                
+                <Flex align={'center'}>
+                    <Box mr={'4'} textAlign={'right'}>
+                        <Text>Vanderson Avellar</Text>
+                        <Text color={'gray.400'} fontSize={'small'}>
+                            vandersonavellar1997@gmail.com
+                        </Text>
+                    </Box>
+
+                    <Avatar size='md' name='Vanderson Avellar' src="https://github.com/avellar2.png"/>
+                </Flex>
             </Flex>
         </Flex>
     )
