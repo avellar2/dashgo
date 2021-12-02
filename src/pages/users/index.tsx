36785e1648e688766738fe06bd7c114a65a061c5
Vanderson Avellar <vandimavellar1997@gmail.com>
2021-12-02 00:47:29 -0300

    Componente: Pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -15,6 +15,7 @@ import {
 } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
@@ -81,8 +82,94 @@ export default function UserList() {
                                     </Button>
                                 </Td>
                             </Tr>
+
+                            <Tr>
+                                <Td px={"6"}>
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight={"bold"}>
+                                            Vanderson Avellar
+                                        </Text>
+                                        <Text fontSize={"sm"} color="gray.300">
+                                            vandersonavellar1997@gmail.com
+                                        </Text>
+                                    </Box>
+                                </Td>
+                                <Td>28 de setembro, 2021</Td>
+                                <Td>
+                                    <Button
+                                        as='a'
+                                        size='sm'
+                                        fontSize='sm'
+                                        colorScheme={'purple'}
+                                        leftIcon={<Icon as={RiPencilLine} fontSize='16'/>}
+                                    >
+                                        Editar
+                                    </Button>
+                                </Td>
+                            </Tr>
+
+                            <Tr>
+                                <Td px={"6"}>
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight={"bold"}>
+                                            Vanderson Avellar
+                                        </Text>
+                                        <Text fontSize={"sm"} color="gray.300">
+                                            vandersonavellar1997@gmail.com
+                                        </Text>
+                                    </Box>
+                                </Td>
+                                <Td>28 de setembro, 2021</Td>
+                                <Td>
+                                    <Button
+                                        as='a'
+                                        size='sm'
+                                        fontSize='sm'
+                                        colorScheme={'purple'}
+                                        leftIcon={<Icon as={RiPencilLine} fontSize='16'/>}
+                                    >
+                                        Editar
+                                    </Button>
+                                </Td>
+                            </Tr>
+
+                            <Tr>
+                                <Td px={"6"}>
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight={"bold"}>
+                                            Vanderson Avellar
+                                        </Text>
+                                        <Text fontSize={"sm"} color="gray.300">
+                                            vandersonavellar1997@gmail.com
+                                        </Text>
+                                    </Box>
+                                </Td>
+                                <Td>28 de setembro, 2021</Td>
+                                <Td>
+                                    <Button
+                                        as='a'
+                                        size='sm'
+                                        fontSize='sm'
+                                        colorScheme={'purple'}
+                                        leftIcon={<Icon as={RiPencilLine} fontSize='16'/>}
+                                    >
+                                        Editar
+                                    </Button>
+                                </Td>
+                            </Tr>
                         </Tbody>
                     </Table>
+
+                    <Pagination/>
                 </Box>
             </Flex>
         </Box>
