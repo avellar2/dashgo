e8ca7d0914f47c40a3d1f4dc2d3601a00fa57864
Vanderson Avellar <vandimavellar1997@gmail.com>
2021-12-14 12:25:15 -0300

    Prefetch de dados

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -15,6 +15,8 @@ import { Sidebar } from "../../components/Sidebar";
 import * as yup from 'yup'
 import { SubmitHandler, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup/dist/yup";
+import { useMutation } from "react-query";
+import { api } from "../../services/api";
 
 type CreateUserFormData = {
     name: string,
@@ -34,6 +36,17 @@ const createUserFormSchema = yup.object().shape({
 
 export default function CreateUser() {
 
+    const createUser = useMutation(async (user: CreateUserFormData) => {
+        const response = await api.post('users', {
+            user: {
+                ...user,
+                created_at: new Date()
+            }
+        })
+
+        return response.data.user 
+    })
+
     const {register, handleSubmit, formState} = useForm({
         resolver: yupResolver(createUserFormSchema)
     })
@@ -41,8 +54,7 @@ export default function CreateUser() {
     const {errors} = formState;
 
     const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-        await new Promise(resolve => setTimeout(resolve, 2000))
-        console.log(values)
+        await createUser.mutateAsync(values)
     }
 
     return (
